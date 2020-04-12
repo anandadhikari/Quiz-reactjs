2719301581b9efd8502c40040c05c7d1f0d2ecd6
Anand <anandadhikari02@gmail.com>
2020-04-12 18:21:40 +0530

    first commit

---
+++ app/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Footer extends React.Component{
+    render() {
+        return (
+            <footer>
+                <small>
+                    You can find the code repo on <a href="https://github.com/anandadhikari?tab=repositories">Github</a>. Feel free to make any pull requests to improve the application.
+                </small>
+                <p>Built with <span>&hearts;</span> by <a href="https://github.com/anandadhikari?tab=repositories">Anand Adhikari</a></p>
+            </footer>
+        )
+    }
+}
+
+export default Footer
